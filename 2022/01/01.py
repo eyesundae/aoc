7bfda6df11adc1405fa9c37eafc1f3766482b414
eyesundae <talvitre@gmail.com>
2022-12-01 19:21:41 -0800

    Added: part 02

--- 2022/01/01.py
+++ 2022/01/01.py
@@ -3,12 +3,16 @@
 
 def main():
 	elves = []
-	for x in open("AoC1.txt").read().split("\n\n"):
+	for x in open("AoC1-2.txt").read().split("\n\n"):
 		thisElf = (sum(list(map(int, x.split()))))
 		elves.append(thisElf)
 
-	print("Fatty ELF number ", 	elves.index(max(elves))+1, 
+	print("Fatty ELF number ",  elves.index(max(elves))+1, 
 	", they've got ", max(elves), " calories!")
+	
+	elves.sort(reverse=True)
+	print("Top 3 elves carrying: ", 
+	sum(elves[:3]), " calories")
 	
 if __name__ == "__main__":
 	main()
