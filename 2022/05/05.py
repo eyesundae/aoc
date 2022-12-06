bd0a0e3abd09c568e883d0da3ad8dbc04842887d
eyesundae <talvitre@gmail.com>
2022-12-05 21:47:12 -0800

    naive 05

---
+++ 2022/05/05.py
@@ -0,0 +1,76 @@
+#Advent of Code
+#Day 5: Supply Stacks
+from fileinput import input
+from pprint import pprint
+import re
+import time
+
+floorMap, crates, doMove = [], [], []
+working = 0
+
+def flipOrientation(crates):
+	return (list(map(list,zip(*crates))))
+
+#truncate and remove empty spaces
+def fixCargo (cargo):
+	for x in cargo:
+		for y in x:
+			if not re.search('[a-zA-Z]', y):
+				x.remove(y)
+
+def CM9000(x):
+	print("WORKING", x.strip())
+	if len(x.strip()) > 1:
+		doMove = re.findall(r'\d+', x.strip())
+		#print("doMove", doMove)
+		for nummer in range(int(doMove[0])):
+			temp = crates[int(doMove[1]) - 1].pop()
+			crates[int(doMove[2]) - 1].append(temp)
+
+def CM9001(x):
+	print("WORKING", x.strip())
+	if len(x.strip()) > 1:
+		doMove = re.findall(r'\d+', x.strip())
+		#print("doMove", doMove)
+		#difference from PART 1 here
+		temp = len(crates[int(doMove[1]) - 1]) - int(doMove[0]) 
+		for nummer in range(int(doMove[0])):
+			#pop moving list
+			crates[int(doMove[2]) - 1].append\
+			(crates[int(doMove[1] ) - 1].pop(temp))
+
+for x in input():
+	if x == "\n":
+		working = 1
+		#lol put that here
+		fixCargo(crates)
+		fixCargo(crates)
+		fixCargo(crates)
+		
+	if not working:
+		#parse crates
+		print("NOT WORKING\n", x.strip())
+		cargoStack = []
+		searchValue = 1
+		while searchValue < len(x):
+			cargoStack.append(x[searchValue])
+			searchValue += 4
+		cargoStack.reverse()
+		floorMap.append(cargoStack)
+		crates = flipOrientation(floorMap)
+		crates.reverse()
+		for z in crates:
+			z.reverse()
+		
+	else:
+		#move crates
+		#PART 1
+		CM9000(x)
+
+		#PART 2
+		#CM9001(x)
+
+		pprint(crates)
+		print("\n")
+		#time.sleep(1)
+			
